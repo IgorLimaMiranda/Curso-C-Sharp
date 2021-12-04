bf10ae6bfca5f9335c51e4672864056a80201b82
Igor Lima <ig.lm123@gmail.com>
2021-12-04 09:21:16 -0400

    Programa Console criado.

---
+++ Curso-C-Sharp/Olá Mundo/App.cs
@@ -0,0 +1,11 @@
+﻿public class App
+{
+    // Atributos de uma classe ou campos da classe
+    // [Modificador de acesso] [tipo atributo] <tipo da variável> <identificador>
+    public int varInt;
+    static void Main()
+    {
+        // Variável locais
+        int varLocal;
+    }
+}
