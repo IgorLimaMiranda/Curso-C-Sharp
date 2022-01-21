a1c57c68e08d263ee644395818b15466c1232230
Igor Lima <ig.lm123@gmail.com>
2022-01-21 12:28:10 -0400

    Strings e a classe System.Text.StringBuilder

---
+++ Curso-C-Sharp/Olá Mundo/stringBuilderEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//namespace da classe StringBuilder
+using System.Text;
+public class stringBuilderEx
+{
+    static void Main()
+    {
+        // Criando um string Builder
+        StringBuilder strB =
+        new StringBuilder("Este ... um teste");
+        // Convertendo o StringBuilder para string com
+        //o método ToString()
+        Console.WriteLine
+        ("StringBuilder: {0}", strB.ToString());
+        // Mostrando no console a capacidade do
+        // StringBuilder com o método
+        // strB.Capacity.ToString( )
+        Console.WriteLine
+        ("{0}", strB.Capacity.ToString());
+        // Inserindo uma string qualquer na posiçao 8
+        // do stringBuilder.
+        strB.Insert(8, " é");
+        Console.WriteLine("StringBuilder: {0}", strB.ToString());
+        Console.ReadLine();
+    }
+}
---
+++ Curso-C-Sharp/Olá Mundo/strings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class strings
+{
+    static void Main()
+    {
+        string str = "Strings em C#";
+        string concatStr;
+        string novoStr;
+        Console.WriteLine("Strings de exemplo: {0}", str);
+        // concatenando strings
+        concatStr = str + " e .NET";
+        Console.WriteLine
+        ("Strings de exemplo: {0}", concatStr);
+        // Mostrando o tamanho do string com o método //Length
+        Console.WriteLine
+        ("Tamanho do string concatStr: {0}",
+        concatStr.Length.ToString());
+        // Extraindo um sub-string como o método
+        //Substring
+        Console.WriteLine
+        ("Sub-string de concatStr: {0}",
+        concatStr.Substring(0, 7));
+        // Comparando strings com o operador "=="
+        //sobre-carregado
+        if (concatStr == str)
+            Console.WriteLine("Strings iguais");
+        else
+            Console.WriteLine("Strings diferentes");
+        Console.ReadLine();
+    }
+}
